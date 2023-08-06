aa776fe0318cc7d2ff72592b3d193ac337f75387
Varun Barad <varun@varunbarad.com>
2023-08-06 07:47:15 +0530

    Add a table to show summary of stats across different paragraphs

--- stats.js
+++ stats.js
@@ -1,14 +1,94 @@
 "use strict";
 
-let graph = null;
 document.addEventListener("DOMContentLoaded", function () {
-  const container = document.getElementById("container");
+  const records = getRecords();
+  updateGraph(records);
+  updateTable(records);
+});
+
+const updateTable = (records) => {
+  const tableBody = document.querySelector('#stats-table > tbody');
+  tableBody.innerHTML = '';
   
+  if (records.length === 0) {
+    const row = document.createElement('tr');
+    const emptyCell = document.createElement('td');
+    emptyCell.colSpan = 8;
+    emptyCell.innerText = 'No records yet.';
+    row.appendChild(emptyCell);
+    tableBody.appendChild(row);
+  } else {
+    const groupedRecords = records.reduce((groups, record) => {
+      if (!groups[record.paragraphIndex]) {
+        groups[record.paragraphIndex] = {
+          paragraphIndex: record.paragraphIndex,
+          records: [],
+        };
+      }
+      groups[record.paragraphIndex].records.push(record);
+      
+      return groups;
+    }, []);
+    
+    const summarisedRecords = groupedRecords.map((group) => {
+      return {
+        paragraphIndex: group.paragraphIndex,
+        numberOfAttempts: group.records.length,
+        bestTimeInSeconds: Math.min(...group.records.map(record => record.timeTakenInSeconds)),
+        worstTimeInSeconds: Math.max(...group.records.map(record => record.timeTakenInSeconds)),
+        averageTimeInSeconds: Math.floor(group.records.reduce((sum, record) => sum + record.timeTakenInSeconds, 0) / group.records.length),
+        bestWordsPerMinute: Math.min(...group.records.map(record => record.wordsPerMinute)),
+        worstWordsPerMinute: Math.max(...group.records.map(record => record.wordsPerMinute)),
+        averageWordsPerMinute: Math.floor(group.records.reduce((sum, record) => sum + record.wordsPerMinute, 0) / group.records.length),
+      };
+    });
+    
+    summarisedRecords.forEach((recordGroup) => {
+      const row = document.createElement('tr');
+      
+      const cellParagraphIndex = document.createElement('td');
+      cellParagraphIndex.innerText = `${parseInt(recordGroup.paragraphIndex) + 1}`;
+      row.appendChild(cellParagraphIndex);
+      
+      const cellNumberOfAttempts = document.createElement('td');
+      cellNumberOfAttempts.innerText = `${recordGroup.numberOfAttempts}`;
+      row.appendChild(cellNumberOfAttempts);
+      
+      const cellBestTime = document.createElement('td');
+      cellBestTime.innerText = `${formattedTime(recordGroup.bestTimeInSeconds)}`;
+      row.appendChild(cellBestTime);
+      
+      const cellWorstTime = document.createElement('td');
+      cellWorstTime.innerText = `${formattedTime(recordGroup.worstTimeInSeconds)}`;
+      row.appendChild(cellWorstTime);
+      
+      const cellAverageTime = document.createElement('td');
+      cellAverageTime.innerText = `${formattedTime(recordGroup.averageTimeInSeconds)}`;
+      row.appendChild(cellAverageTime);
+      
+      const cellBestWordsPerMinute = document.createElement('td');
+      cellBestWordsPerMinute.innerText = `${recordGroup.bestWordsPerMinute}`;
+      row.appendChild(cellBestWordsPerMinute);
+      
+      const cellWorstWordsPerMinute = document.createElement('td');
+      cellWorstWordsPerMinute.innerText = `${recordGroup.worstWordsPerMinute}`;
+      row.appendChild(cellWorstWordsPerMinute);
+      
+      const cellAverageWordsPerMinute = document.createElement('td');
+      cellAverageWordsPerMinute.innerText = `${recordGroup.averageWordsPerMinute}`;
+      row.appendChild(cellAverageWordsPerMinute);
+      
+      tableBody.appendChild(row);
+    });
+  }
+};
+
+let graph = null;
+const updateGraph = (records) => {
   const canvasElement = document.getElementById("chart");
   const color = getComputedStyle(canvasElement).getPropertyValue('--color-success');
   const colorTranslucent = getComputedStyle(canvasElement).getPropertyValue('--color-success-translucent');
   
-  const records = getRecords();
   if (graph !== null) {
     graph.destroy();
   }
@@ -48,7 +128,7 @@ document.addEventListener("DOMContentLoaded", function () {
       },
     },
   });
-});
+};
 
 const getRecords = () => {
   const database = JSON.parse(localStorage.getItem("database"));
